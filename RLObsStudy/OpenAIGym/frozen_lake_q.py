761648a71b29be2253fa8257333d05948385b266
AffectResearch <b.hilpert@liacs.leidenuniv.nl>
2024-02-22 18:51:46 +0100

    added an RL algorithm to the code

--- RLObsStudy/OpenAIGym/frozen_lake_q.py
+++ RLObsStudy/OpenAIGym/frozen_lake_q.py
@@ -1,21 +1,57 @@
 import gymnasium as gym
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle
 
-def run():
-    env = gym.make("FrozenLake-v1", map_name="8x8", is_slippery=True, render_mode="human")
+def run(episodes, render=False):
+    env = gym.make("FrozenLake-v1", map_name="8x8", is_slippery=False, render_mode="human" if render else None)
 
-    state = env.reset()[0] #states: 0 to 63, 0=topleft corner, 63 = bottom right corner
-    terminated = False # True, when fall in hole or reached goal
-    truncated = False # True when actions > 200
+    q = np.zeros((env.observation_space.n, env.action_space.n)) # init a 64 x 4 array
 
-    while(not terminated and not truncated):
+    learning_rate_a = 0.9 # alpha/learning rate
+    discount_factor_g = 0.9 # gamma or discount factor
 
-        action = env.action_space.sample() # actions: 0=left, 1=down, 2=right, 3=up
+    epsilon = 1 # 1 = 100% random actions
+    epsilon_decay_rate = 0.0001 # epsilon decay rate 1/0.0001 = 10000
+    rng = np.random.default_rng()
 
-        new_state, reward,terminated,truncated,_ = env.step(action)
+    rewards_per_episode = np.zeros(episodes)
 
-        state = new_state
+    for i in range(episodes):
+        state = env.reset()[0] #states: 0 to 63, 0=topleft corner, 63 = bottom right corner
+        terminated = False # True, when fall in hole or reached goal
+        truncated = False # True when actions > 200
+
+        while(not terminated and not truncated):
+            if rng.random() < epsilon:
+                action = env.action_space.sample() # actions: 0=left, 1=down, 2=right, 3=up
+            else:
+                action = np.argmax(q[state,:])
+
+            new_state, reward,terminated,truncated,_ = env.step(action)
+
+            q[state,action] = q[state,action] + learning_rate_a * (
+                reward + discount_factor_g * np.max(q[new_state,:]) - q[state,action]
+            )
+
+            state = new_state
+    
+    epsilon = max(epsilon - epsilon_decay_rate, 0)
+
+    if(epsilon==0):
+        learning_rate_a = 0.0001
 
     env.close()
 
+    sum_rewards = np.zeros(episodes)
+    for t in range(episodes):
+        sum_rewards[t] = np.sum(rewards_per_episode[max(0,t-100):(t+1)])
+    plt.pot(sum_rewards)
+    plt.savefig("frozen_lake8x8.png")
+
+    f = open("frozen_lake8x8.pk1","wb")
+    pickle.dump(q,f)
+    f.close()
+
 if __name__ == "__main__":
-    run() 
+    run(15000) 
